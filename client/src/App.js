d45e132d3ad742f2ffd47340e15619e687a22792
Sara <sara.m.adamski@gmail.com>
2021-08-05 19:08:12 -0500

    connect to front end react

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+// ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
+// ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
+// InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
+// createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
+
+// establish a new link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// instantiate the Apollo Client instance
+const client = new ApolloClient({
+  // create the connection to the API endpoint
+  link: httpLink,
+  // instantiate a new cache object
+  cache: new InMemoryCache(),
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -23,6 +24,14 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
